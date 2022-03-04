09d8deb426ffc962a58aeef0bf99b5c77e3c7447
rohitsdm <rohitk.kumar@smartdatainc.net>
2022-03-05 02:15:29 +0530

    add option in db connect

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors())
 app.use(express.json())
 var path = require('path');
 
-mongoose.connect( process.env.MONGODB_URI );
+mongoose.connect( process.env.MONGODB_URI, { useNewUrlParser: true } );
 
 app.use('/', express.static(path.join(__dirname, './client/build')));
 
