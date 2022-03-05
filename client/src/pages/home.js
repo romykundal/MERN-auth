83bf439fb0d0d4fc97872a271410165616cabf5d
rohitsdm <rohitk.kumar@smartdatainc.net>
2022-03-05 06:18:37 +0530

    latest build react

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -5,7 +5,7 @@ const Home = () => {
 
 	return (
 		<div className="navbar-start">
-            <h1><NavLink className="navbar-item" activeClassName="is-active" to="/">
+            <h3><NavLink className="navbar-item" activeClassName="is-active" to="/">
               Home
             </NavLink>
 			| 
@@ -23,7 +23,7 @@ const Home = () => {
               to="/register"
             >
               Sign up
-            </NavLink></h1>
+            </NavLink></h3>
     </div>
 	)
 }
